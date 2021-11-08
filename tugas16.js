6f71866e01dac31dea490f2acbe39cb6f567fa5d
whoisraa <raainvictus@gmail.com>
2021-11-08 08:07:26 +0700

    tugas 16 js

---
+++ tugas16.js
@@ -0,0 +1,9 @@
+function deret() {
+    let nilaiMin = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    let nilaiMaks = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+    console.log(`Nilai Minimal : ${nilaiMin}`);
+    console.log(`Nilai Maksimal : ${nilaiMaks}`);
+}
+
+deret();
